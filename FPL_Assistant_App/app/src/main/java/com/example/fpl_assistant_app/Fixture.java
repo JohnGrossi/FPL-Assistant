4b85e0de20ca59edfb34b876761a61e86cfd8bb4
john grossi <johngrossi1999@hotmail.com>
2021-02-03 13:38:06 +0000

    initial AS commit

---
+++ FPL_Assistant_App/app/src/main/java/com/example/fpl_assistant_app/Fixture.java
@@ -0,0 +1,14 @@
+package com.example.fpl_assistant_app;
+
+public class Fixture {
+    public String fixtureName;
+
+    public void setFixtureName(String fixtureName) {
+        this.fixtureName = fixtureName;
+    }
+
+    public String getFixtureName() {
+        return fixtureName;
+    }
+
+}
---
+++ FPL_Assistant_App/app/src/main/java/com/example/fpl_assistant_app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.fpl_assistant_app;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+
+        Set<Integer> topLevelDestinations = new HashSet<>();
+        topLevelDestinations.add(R.id.playerComparisonFragment);
+        topLevelDestinations.add(R.id.fixturesFragment);
+        topLevelDestinations.add(R.id.totwFragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(topLevelDestinations).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+
+    public void switchThis (View v) {
+
+    }
+}
