db4c8b5b6f39af53e283aa4d3d440129e88a1568
John <johngrossi1999@hotmail.com>
2021-02-18 16:01:25 +0000

    gets players. shows all data

--- FPL_Assistant_App/app/src/main/java/com/example/fpl_assistant_app/Main/PlayerComparisonFragment.java
+++ FPL_Assistant_App/app/src/main/java/com/example/fpl_assistant_app/Main/PlayerComparisonFragment.java
@@ -1,17 +1,22 @@
 package com.example.fpl_assistant_app.Main;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.fpl_assistant_app.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,9 +26,16 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,6 +44,13 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class PlayerComparisonFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    String[] playerArray;
+    Spinner spinner11;
+    ArrayAdapter<CharSequence> adapter2 = null;
+    Activity thisActivity;
+    String team;
+    String[] playerStat = {"team", "price", "position", "fitness", "lastFixture", "secondLastFixture", "thirdLastFixture", "lastPoints", "secondLastPoints", "thirdLastPoints", "nextFixture", "secondNextFixture", "thirdNextFixture", "pointsTotal", "cleanSheetTotal", "penaltySaves", "goalsTotal", "assistTotal"};
 
     public PlayerComparisonFragment() {
         // Required empty public constructor
@@ -60,35 +79,113 @@ public class PlayerComparisonFragment extends Fragment implements AdapterView.On
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_player_comparison, container, false);
+        FragmentActivity context = getActivity();
+        thisActivity = this.getActivity();
         String teams[] = getResources().getStringArray(R.array.teams);
 
         Spinner spinner = (Spinner) view.findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this.getActivity(), android.R.layout.simple_spinner_item, teams);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(adapter);
+        spinner11 = (Spinner) view.findViewById(R.id.spinner11);
         spinner.setOnItemSelectedListener(this);
+        spinner11.setOnItemSelectedListener(this);
 
         return view;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.d("big man tag", "got HERE");
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference collectionRef = db.collection("teams").document(((String) parent.getItemAtPosition(position)).toUpperCase()).collection("players");
-        Log.d("HERE", (String) parent.getItemAtPosition(position));
-        collectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
-                    for(DocumentSnapshot snapshot: snapshotList) {
-                        Log.d("big man tag", "onSuccess: " + snapshot.getId());
-                    }
-            }
-        });
+        setStats(parent, position);
     }
+
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void setStats(AdapterView<?> parent, int position) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        if (parent.getId() == R.id.spinner1) {
+            team = (String) parent.getItemAtPosition(position);
+            CollectionReference collectionRef = db.collection("teams").document(((String) parent.getItemAtPosition(position)).toUpperCase()).collection("players");
+            collectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
+                    ArrayList<String> players = new ArrayList<>();
+                    for (DocumentSnapshot snapshot : snapshotList) {
+                        //Log.d("big man tag", "onSuccess: " + snapshot.getId());
+                        players.add(snapshot.getId());
+                    }
+
+                    playerArray = new String[players.size()];
+                    playerArray = players.toArray(playerArray);
+                    adapter2 = new ArrayAdapter<CharSequence>(thisActivity, android.R.layout.simple_spinner_item, playerArray);
+
+                    adapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+                    spinner11.setAdapter(adapter2);
+                }
+            });
+        }
+        if (parent.getId() == R.id.spinner11) {
+            String player = (String) parent.getItemAtPosition(position);
+            setPhoto(player, team.toUpperCase());
+            DocumentReference docRef = db.collection("teams").document(team.toUpperCase()).collection("players").document(player);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    DocumentSnapshot document = task.getResult();
+                    for (String i : playerStat) {
+                        TextView textView;
+
+                        int resID = getResources().getIdentifier(i, "id", "com.example.fpl_assistant_app");
+                        textView = getView().findViewById(resID);
+
+                        if(textView.getResources().getResourceName(textView.getId()).contains("pointsTotal")) {
+                            textView.setText("Points:" +String.valueOf(document.get(i)));
+                        }else if(textView.getResources().getResourceName(textView.getId()).contains("cleanSheetTotal")) {
+                            textView.setText("Clean Sheets:" +String.valueOf(document.get(i)));
+                        }else if(textView.getResources().getResourceName(textView.getId()).contains("penaltySaves")) {
+                            textView.setText("Penalty Saves:" +String.valueOf(document.get(i)));
+                        }else if(textView.getResources().getResourceName(textView.getId()).contains("goalsTotal")) {
+                            textView.setText("Goals:" +String.valueOf(document.get(i)));
+                        }else if(textView.getResources().getResourceName(textView.getId()).contains("assistTotal")) {
+                            textView.setText("Assists:" +String.valueOf(document.get(i)));
+                        } else {
+                            textView.setText(String.valueOf(document.get(i)));
+
+                        }
+
+                        if (textView.getText().toString().contains("null")) {
+                            textView.setText("");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public void setPhoto(String player, String team) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        String filename = player +team + ".png";
+        StorageReference storageReference = storage.getReferenceFromUrl("gs://fpl-assistant-41263.appspot.com/Pics").child(filename);
+
+        try {
+            final File picture = File.createTempFile(filename, "png");
+            storageReference.getFile(picture).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    int picID = getResources().getIdentifier("Player1Image", "id", "com.example.fpl_assistant_app");
+                    Bitmap bitmap = BitmapFactory.decodeFile(picture.getAbsolutePath());
+                    ImageView imageView;
+                    imageView = getView().findViewById(picID);
+                    imageView.setImageBitmap(bitmap);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
